bb9384fc2dba6ad6f77e945b2136602a7a023228
mounirzer <38168068+mounirzer@users.noreply.github.com>
2018-12-07 22:32:19 +0100

    bibarosa

---
+++ sidebar.php
@@ -0,0 +1,55 @@
+<div class="left-sidebar-pro">
+        <nav id="sidebar" class="">
+            <div class="sidebar-header">
+                <a href="index.html"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+                <strong><a href="index.html"><img src="img/logo/logosn.png" alt="" /></a></strong>
+            </div>
+            <div class="left-custom-menu-adp-wrap comment-scrollbar">
+                <nav class="sidebar-nav left-sidebar-menu-pro">
+                    <ul class="metismenu" id="menu1">
+                        <li class="active">
+                            <a  href="index.php">
+								   <span class="educate-icon educate-home icon-wrap"></span>
+								   <span class="mini-click-non">Home</span>
+								</a>
+                            
+                        </li>
+                        <li>
+                            <a title="Landing Page" href="calendrier.php" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Calandrie</span></a>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="all-professors.html" aria-expanded="false"><span class="educate-icon educate-professor icon-wrap"></span> <span class="mini-click-non">Professor</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                                <li><a title="Edit Professor" href="edit-profil.php"><span class="mini-sub-pro">Edit Profil</span></a></li>
+                                
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="all-students.html" aria-expanded="false"><span class="educate-icon educate-student icon-wrap"></span> <span class="mini-click-non">Students</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                                <li><a title="All Students" href="all-stu.php"><span class="mini-sub-pro">All Students</span></a></li>
+                                
+                                <li><a title="Edit Students" href="edit-student.html"><span class="mini-sub-pro">Edit</span></a></li>
+                                
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow" href="all-courses.html" aria-expanded="false"><span class="educate-icon educate-library icon-wrap"></span> <span class="mini-click-non">Education</span></a>
+                            <ul class="submenu-angle" aria-expanded="false">
+                                <li><a title="All Library" href="library-assets.html"><span class="mini-sub-pro">Add Question</span></a></li>
+                                <li><a title="Add Library" href="add-library-assets.html"><span class="mini-sub-pro">Add articles</span></a></li>
+                                
+                            </ul>
+                        </li>
+                        
+                        
+                        
+                        
+                        
+                       
+                        
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+    </div>
